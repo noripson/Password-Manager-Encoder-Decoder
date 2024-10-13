398579506a49d8c829554b784362ac4b071268f1
noripson <77788778+noripson@users.noreply.github.com>
2024-10-13 16:28:05 +0330

    Create Password Manager.py

---
+++ Password Manager.py
@@ -0,0 +1,12 @@
+while True:
+    user_input = input("Enter the mode (a)=add | (v)=view | (q)=quit \n")
+    list = ['a', 'v', 'q']
+    
+    if user_input == 'a':
+        pass
+    if user_input == 'v':
+        pass
+    if user_input == 'q':
+        pass
+    else:
+        print("Wrong... Please enter the mode (a)=add | (v)=view | (q)=quit \n")
