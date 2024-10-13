e41e9661c447b4c29d56273f995bc15a9a0e170b
noripson <77788778+noripson@users.noreply.github.com>
2024-10-13 16:45:55 +0330

    Update Password Manager.py

--- Password Manager.py
+++ Password Manager.py
@@ -1,12 +1,21 @@
+def add_passwords(username, password):
+    username=input("your username :") 
+    password=input("your password :")
+    
+
+
+
 while True:
-    user_input = input("Enter the mode (a)=add | (v)=view | (q)=quit \n")
+    user_input = input("Enter the mode (a)=Add | (v)=View | (q)=Quit \n")
     list = ['a', 'v', 'q']
     
     if user_input == 'a':
+        print("Your selected Add mode ...")
+        
+        
+    elif user_input == 'v':
         pass
-    if user_input == 'v':
-        pass
-    if user_input == 'q':
+    elif user_input == 'q':
         pass
     else:
-        print("Wrong... Please enter the mode (a)=add | (v)=view | (q)=quit \n")
+        print("Wrong... Please enter the mode (a)=Add | (v)=View | (q)=Quit \n")
